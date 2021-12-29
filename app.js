b763ac75e7c613cd28cb72830666848e8d3e15b4
Aleksandar Nikolic <salex.nikolic@gmail.com>
2021-12-29 09:03:33 +0100

    feat(intervention order) create intervention order documentation 2

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ const limiter = rateLimit({
 
 	// 4) VIEW ENGINE settings
 
+
+
 	// 5) MIDDLEWAREs
 	// 5.1) MIDDLEWAREs for setting a static PUBLIC folder
 app.use(express.static(path.resolve('public')));
