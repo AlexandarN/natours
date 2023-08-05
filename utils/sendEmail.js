2ff119ee67f18ce11facf6cbe5d26c0f9e84dc95
Aleksandar Nikolic <salex.nikolic@gmail.com>
2023-08-05 20:06:55 +0200

    update(transaction); transaction helper updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,6 +7,8 @@ const nodemailer = require('nodemailer');
 const env = require('../config/env');
 
 // 3) CONSTANTs
+
+
 // 4) MIDDLEWAREs
 module.exports = async options => {
   // 1. Create transporter
